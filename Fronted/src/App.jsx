be8cffa20e75aa8feddfab0cdd6663f9db277a1b
patel-Asan <patelasan91@example.com>
2026-05-06 22:06:01 +0530

    fix: correct Home.jsx import case sensitivity for Vercel build

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Routes, Route, useLocation } from "react-router-dom"
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
