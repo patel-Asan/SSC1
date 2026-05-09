614bff33fde8107673e2dd0f393ac4e634101805
patel-Asan <patelasan91@example.com>
2026-05-09 20:06:14 +0530

    feat: add PWA support with manifest, service worker, and installable app config

--- Fronted/src/main.jsx
+++ Fronted/src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 import{BrowserRouter} from 'react-router-dom'
 import ShopcontextProvider from './context/shopcontext.jsx'
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
+}
+
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
  <ShopcontextProvider>
